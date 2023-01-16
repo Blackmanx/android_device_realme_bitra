69a4e57aac1624963baa4e7ffacee22688284836
Blackmanx <blackman@sajl.tk>
2023-01-16 01:31:43 +0100

    bitra: Improve vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,10 @@
 echo "Applying Brightness patch"
 
 cd frameworks/base || exit 1
-git fetch https://review.arrowos.net/ArrowOS/android_frameworks_base refs/changes/88/17688/1 && git cherry-pick FETCH_HEAD
+if git log -150 --oneline --decorate | grep "Fix brightness slider curve for some devices"; then
+	echo "Fix was already applied"
+else
+	git fetch https://review.arrowos.net/ArrowOS/android_frameworks_base refs/changes/88/17688/1 && git cherry-pick FETCH_HEAD
+fi
+
 cd ../..
