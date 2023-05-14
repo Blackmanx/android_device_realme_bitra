4ad99b7803f8dd7a3bbd54328367ab6e466697c5
Mashopy <eliasgheeraert@gmail.com>
2023-05-14 21:12:53 +0200

    bitra: init: Move IRQs tuning to device tree [2/4]
    Change-Id: Ic068bc487d1857534e8596ef3997213261785498

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    init.oplus.perf.rc
+
 # Overlays
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
